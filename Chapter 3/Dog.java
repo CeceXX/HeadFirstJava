bc6f63b7cc28e45898948c1d72e3736d9a4d2434
Cesare de Cal <hi@cesare.io>
2016-01-17 20:00:43 +0100

    Completed Dog exercise

--- Chapter 3/Dog.java
+++ Chapter 3/Dog.java
@@ -15,5 +15,24 @@ class Dog {
 		myDogs[2] = dog1;
 		
 		// now access the Dogs using the array references
+		myDogs[0].name = "Fred";
+		myDogs[1].name = "Merge";
+		
+		// Hmmm... what is myDogs[2] name?
+		System.out.print("last dog's name is ");
+		System.out.println(myDogs[2].name);
+		
+		// now loop through the array and tell all dogs to bark
+		int x = 0;
+		while (x < myDogs.length) {
+			myDogs[x].bark();
+			x++;
+		}
 	}
+	
+	public void bark() {
+		System.out.println(name + " says Ruff!");
+	}
+	public void eat() { }
+	public void chaseCat() { }
 }
